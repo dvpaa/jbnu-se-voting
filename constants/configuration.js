1e48210af213b65b0c73efc65bfc43e314300ad8
Geonu Kim <kgo000926@gmail.com>
2023-09-09 04:37:05 +0900

    완료 ㅋ

--- constants/configuration.js
+++ constants/configuration.js
@@ -4,18 +4,18 @@ import { monthMapper } from "./mapper";
 const CONFIGS = {
     YEAR: "2023",
     VOTE_COUNTER: "김재현(개표위원장)",
-    MAIN_CANDIDATE: "19학번 정용한",
+    MAIN_CANDIDATE: "21학번 전정현",
     SUB_CANDIDATE: "19학번 김건우",
     POSTER_URL: "/images/candidate_poster.png",
-    USER_NUMBERS: 1,
+    USER_NUMBERS: 33,
     
     // 선거 종료 일시 설정
     // 입력 표준은 KST
     END_YEAR: 2023,
     END_MONTH: monthMapper.SEP, // month base = 0
-    END_DATE: 8,
-    END_HOUR: 23,
-    END_MINUTE: 27,
+    END_DATE: 9,
+    END_HOUR: 13,
+    END_MINUTE: 0,
 };
 
 module.exports = {
--- constants/messages.js
+++ constants/messages.js
@@ -9,7 +9,7 @@ const ERRORS = {
 const INFO = {
     ELECTION_INFO: "단일 후보 찬성이 2/3 넘었으므로 정후보 " + configs.MAIN_CANDIDATE + ", 부후보 " + configs.SUB_CANDIDATE + "이 당선되었습니다.",
     ABORTION_INFO: "단일 후보 찬성이 2/3을 넘지 않았으므로 정후보 " + configs.MAIN_CANDIDATE + ", 부후보 " + configs.SUB_CANDIDATE + "이 낙선되었습니다.",
-    END_INFO: '투표에 참여해주셔서 감사합니다. ' + configs.END_YEAR + '년 ' + configs.END_MONTH + '월 ' + configs.END_DATE + '일 ' + configs.END_HOUR + ':' + configs.END_MINUTE + ' 이후 결과가 공개됩니다.'
+    END_INFO: '투표에 참여해주셔서 감사합니다. ' + configs.END_YEAR + '년 ' + (configs.END_MONTH + 1) + '월 ' + configs.END_DATE + '일 ' + configs.END_HOUR + ':' + configs.END_MINUTE + ' 이후 결과가 공개됩니다.'
 };
 
 module.exports = {
